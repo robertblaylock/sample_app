c5559d64ee8170a984ca4a2acef8f368dbe1dc1a
Robert Blaylock <robert@teamusawi.com>
2011-02-19 02:08:25 -0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"  
   get "pages/info"  
   get "pages/help"
+  get "users/new"
   
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/info', :to => 'pages#info'
+  
+  match '/signup', :to => 'users#new'
   
   root :to => 'pages#home'
   
